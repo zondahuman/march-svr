a6cbd61abbcd8ea912c5a788d1e6272324aaf60e
zondahuman <zondahuman@gmail.com>
2017-07-25 19:53:08 +0800

    add

---
+++ march-concurrent/src/main/java/com/abin/lee/march/svr/concurrent/communicate/CountDownLatchMode.java
@@ -0,0 +1,15 @@
+package com.abin.lee.march.svr.concurrent.communicate;
+
+/**
+ * Created by abin on 2017/7/25 19:52.
+ * march-svr
+ * com.abin.lee.march.svr.concurrent.communicate
+ * http://blog.csdn.net/habren/article/details/51749535
+ */
+public class CountDownLatchMode {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
