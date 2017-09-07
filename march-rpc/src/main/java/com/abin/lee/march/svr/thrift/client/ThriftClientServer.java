4ac88ddb6968e359eed6029a72bb6011991dc395
zondahuman <zondahuman@gmail.com>
2017-09-07 16:31:35 +0800

    add thrift service

--- march-rpc/src/main/java/com/abin/lee/march/svr/thrift/client/ThriftClientServer.java
+++ march-rpc/src/main/java/com/abin/lee/march/svr/thrift/client/ThriftClientServer.java
@@ -1,5 +1,16 @@
 package com.abin.lee.march.svr.thrift.client;
 
+import com.abin.lee.march.svr.common.JsonUtil;
+import com.abin.lee.march.svr.thrift.model.TeamInfo;
+import com.abin.lee.march.svr.thrift.service.TeamService;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TMultiplexedProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransportException;
+
+import java.util.List;
+
 /**
  * Created by abin on 2017/9/7 14:20.
  * march-svr
@@ -7,6 +18,25 @@ package com.abin.lee.march.svr.thrift.client;
  */
 public class ThriftClientServer {
 
+    public static void main(String[] args) throws TException {
+        TSocket transport = new TSocket("localhost", 30000);
+        TBinaryProtocol protocol = new TBinaryProtocol(transport);
+        TMultiplexedProtocol teamProtocol = new TMultiplexedProtocol(protocol,"TeamService");
 
-    
+        TeamService.Client teamService = new TeamService.Client(teamProtocol);
+
+//        TMultiplexedProtocol mp2 = new TMultiplexedProtocol(protocol,"UserService");
+//        UserService.Client service2 = newUserService.Client(mp2);
+
+        transport.open();
+        List<TeamInfo> teamInfoList = teamService.findTeamListById(5L);
+//        service2.store1(new User(888,"tom","haha"));
+
+        System.out.println(JsonUtil.toJson(teamInfoList));
+//        System.out.println(service2.retrieve1(999));
+
+        transport.close();
+
+    }
+
 }
--- march-rpc/src/main/java/com/abin/lee/march/svr/thrift/server/ThriftServer.java
+++ march-rpc/src/main/java/com/abin/lee/march/svr/thrift/server/ThriftServer.java
@@ -1,5 +1,14 @@
 package com.abin.lee.march.svr.thrift.server;
 
+import com.abin.lee.march.svr.thrift.service.TeamService;
+import com.abin.lee.march.svr.thrift.service.TeamServiceImpl;
+import org.apache.thrift.TMultiplexedProcessor;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TTransportException;
+
 /**
  * Created by abin on 2017/9/7 14:19.
  * march-svr
@@ -7,7 +16,20 @@ package com.abin.lee.march.svr.thrift.server;
  */
 public class ThriftServer {
 
+    public static void main(String[] args) throws TTransportException {
+        TMultiplexedProcessor processor = new TMultiplexedProcessor();
 
+        TServerTransport t = new TServerSocket(30000);
+        TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(t).processor(processor));
+
+        processor.registerProcessor("TeamService", new TeamService.Processor<TeamService.Iface>(new TeamServiceImpl()));
+//        processor.registerProcessor("UserService", new UserService.Processor<UserService.Iface>(new UserImpl()));
+
+//         TSimpleServer server = new TSimpleServer(new Args(t).processor(processor));
+        System.out.println("the server is started and is listening at 9090...");
+
+        server.serve();
+    }
 
 
 }
