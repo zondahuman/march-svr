169f714e695bf961cf1382800ca033f2f9e8d156
zondahuman <zondahuman@gmail.com>
2017-09-07 14:20:25 +0800

    add

---
+++ march-rpc/src/main/java/com/abin/lee/march/svr/thrift/client/ThriftClientServer.java
@@ -0,0 +1,12 @@
+package com.abin.lee.march.svr.thrift.client;
+
+/**
+ * Created by abin on 2017/9/7 14:20.
+ * march-svr
+ * com.abin.lee.march.svr.thrift.client
+ */
+public class ThriftClientServer {
+
+
+    
+}
---
+++ march-rpc/src/main/java/com/abin/lee/march/svr/thrift/server/ThriftServer.java
@@ -0,0 +1,13 @@
+package com.abin.lee.march.svr.thrift.server;
+
+/**
+ * Created by abin on 2017/9/7 14:19.
+ * march-svr
+ * com.abin.lee.march.svr.thrift.server
+ */
+public class ThriftServer {
+
+
+
+
+}
