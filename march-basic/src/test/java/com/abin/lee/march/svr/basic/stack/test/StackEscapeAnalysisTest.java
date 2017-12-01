55e5fc948426a88a7029a5ea93cb6cbb3a2abaed
zondahuman <zondahuman@gmail.com>
2017-12-01 19:04:33 +0800

    add

--- march-basic/src/test/java/com/abin/lee/march/svr/basic/stack/test/StackEscapeAnalysisTest.java
+++ march-basic/src/test/java/com/abin/lee/march/svr/basic/stack/test/StackEscapeAnalysisTest.java
@@ -1,5 +1,8 @@
 package com.abin.lee.march.svr.basic.stack.test;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+
 /**
  * Created by abin on 2017/7/25 15:20.
  * march-svr
@@ -19,4 +22,16 @@ public class StackEscapeAnalysisTest {
         long e = System.currentTimeMillis();
         System.out.println(e - b);
     }
+
+    @Test
+    public void test1(){
+        int state = 0;
+        while(state < 30){
+            System.out.println("state=" + state + ", state % 3 =" + state % 3);
+            state++;
+        }
+    }
+
+
+
 }
