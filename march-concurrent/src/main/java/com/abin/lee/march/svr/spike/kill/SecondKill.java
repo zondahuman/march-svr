41fadb4c853a110b0a8881b4dc619350c83a1925
zondahuman <zondahuman@gmail.com>
2017-12-06 11:46:47 +0800

    add

--- march-concurrent/src/main/java/com/abin/lee/march/svr/spike/kill/SecondKill.java
+++ march-concurrent/src/main/java/com/abin/lee/march/svr/spike/kill/SecondKill.java
@@ -1,9 +1,27 @@
 package com.abin.lee.march.svr.spike.kill;
 
+import com.google.common.collect.Lists;
+import redis.clients.jedis.Jedis;
+
 /**
  * Created by abin on 2017/12/5 11:58.
  * march-svr
  * com.abin.lee.march.svr.spike.kill
  */
 public class SecondKill {
+
+    public static String host = "172.16.2.146";
+    public static String script = "script.lua";
+
+
+    public static void testTryGetHongBao() throws InterruptedException {
+
+        Jedis jedis = new Jedis(host);
+        String sha = jedis.scriptLoad(script);
+        Object object = jedis.evalsha(sha, Lists.newArrayList(), Lists.newArrayList());
+
+        System.err.println("end:" + System.currentTimeMillis() / 1000);
+    }
+
+
 }
